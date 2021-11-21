250143ae07211da9ddb1ab6f44c4dc47f0e4a8fd
linesoft2 <25446947+linesoft2@users.noreply.github.com>
2021-11-21 21:22:39 +0800

    update

--- 实验二学生选课模拟系统/src/top/linesoft/biptstudy/Main.java
+++ 实验二学生选课模拟系统/src/top/linesoft/biptstudy/Main.java
@@ -26,7 +26,8 @@ class Teacher extends User {
 class OfferCourse {
     Course course;
     Teacher teacher;
-    OfferCourse(Course course,Teacher teacher){
+
+    OfferCourse(Course course, Teacher teacher) {
         this.course = course;
         this.teacher = teacher;
     }
@@ -40,25 +41,23 @@ class Student extends User {
         super(id, name, gender);
     }
 
-    //选择某课
+    // 选择某课
     void selectCourse(OfferCourse offerCourse) {
         course.add(offerCourse);
     }
 
-    //退出某课
-    void exitCourse(OfferCourse offerCourse){
+    // 退出某课
+    void exitCourse(OfferCourse offerCourse) {
         course.remove(offerCourse);
     }
 
-    void printTimetable(){
+    void printTimetable() {
         int i;
-        for(i=0;i<course.size();i++){
-            //包括：编号、课程名称、上课地点、时间、授课教师等
-            System.out.println("编号："+course.get(i).course.id+"、课程名称："
-                    +course.get(i).course.name+"、上课地点："
-                    +course.get(i).course.place+"、时间："
-                    +course.get(i).course.time+"、授课教师："
-                    +course.get(i).teacher.name);
+        for (i = 0; i < course.size(); i++) {
+            // 包括：编号、课程名称、上课地点、时间、授课教师等
+            System.out.println("编号：" + course.get(i).course.id + "、课程名称：" + course.get(i).course.name + "、上课地点："
+                    + course.get(i).course.place + "、时间：" + course.get(i).course.time + "、授课教师："
+                    + course.get(i).teacher.name);
         }
 
     }
@@ -69,7 +68,8 @@ class Course {
     String name;
     String place;
     String time;
-    Course(int id,String name,String place,String time){
+
+    Course(int id, String name, String place, String time) {
         this.id = id;
         this.name = name;
         this.place = place;
@@ -77,31 +77,26 @@ class Course {
     }
 }
 
-
 public class Main {
 
     public static void main(String[] args) {
         List<OfferCourse> offerCourse = new ArrayList<OfferCourse>();
 
-        //教师开设课程操作
-        offerCourse.add(new OfferCourse( new Course(1,"Java","1705","周二上午8点")
-                ,new Teacher(1, "张老师", "男")));
-        offerCourse.add(new OfferCourse( new Course(1,"C语言","1906","周三上午8点")
-                ,new Teacher(1, "王老师", "男")));
-        offerCourse.add(new OfferCourse( new Course(1,"D语言","2006","周四上午8点")
-                ,new Teacher(1, "赵老师", "男")));
-        offerCourse.add(new OfferCourse( new Course(1,"E语言","2106","周五上午8点")
-                ,new Teacher(1, "李老师", "男")));
+        // 教师开设课程操作
+        offerCourse.add(new OfferCourse(new Course(1, "Java", "1705", "周二上午8点"), new Teacher(1, "张老师", "男")));
+        offerCourse.add(new OfferCourse(new Course(1, "C语言", "1906", "周三上午8点"), new Teacher(1, "王老师", "男")));
+        offerCourse.add(new OfferCourse(new Course(1, "D语言", "2006", "周四上午8点"), new Teacher(1, "赵老师", "男")));
+        offerCourse.add(new OfferCourse(new Course(1, "E语言", "2106", "周五上午8点"), new Teacher(1, "李老师", "男")));
 
-        //学生选课操作、退课操作
-        Student student1 = new Student(2,"张三","男");
+        // 学生选课操作、退课操作
+        Student student1 = new Student(2, "张三", "男");
         student1.selectCourse(offerCourse.get(0));
         student1.selectCourse(offerCourse.get(1));
         student1.selectCourse(offerCourse.get(2));
         student1.selectCourse(offerCourse.get(3));
         student1.exitCourse(offerCourse.get(2));
 
-        //打印学生课表信息
+        // 打印学生课表信息
         student1.printTimetable();
     }
 }
